24bc75aeb0bbbf21c0ea9e6739115dcd2e628e2c
Aaron Smith <crypticsymbols@gmail.com>
2014-09-19 17:29:10 -0400

    commit 3

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,15 +1,10 @@
 class FeedbackController < ApplicationController
 
-  # def new
-  #   @message = Feedback.new
-  # end
-
   def create
-
-  	params[:feedback][:email] = current_user.email
+    params[:feedback][:email] = current_user.email
 
     @message = Feedback.new(params[:feedback])
-    
+
     if @message.valid?
       FeedbackMailer.new_feedback(@message).deliver
       redirect_to(root_path, :notice => "Message was successfully sent.")
@@ -18,5 +13,4 @@ class FeedbackController < ApplicationController
       render :new
     end
   end
-
 end
